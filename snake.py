ba80db3e7fb009aef3789d750360942fc980b5e9
Ronak <ronakpatel5177@gmail.com>
2020-06-15 10:19:13 +0530

    Created Head Of Snake

--- snake.py
+++ snake.py
@@ -7,28 +7,34 @@ white = (255, 255, 255)
 red = (255, 0,0)
 black = (0, 0, 0)
 
-screen_width = 800
+screen_width = 900
 screen_height = 600
 # Creating window
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 
+# Game Title
 pygame.display.set_caption("Snake Game by Ronak Patel")
 pygame.display.update()
 
 # Initialize Game Specific Variables
-
 exit_game = False
 game_over = False
+snake_x = 45
+snake_y = 55
+snake_size = 10
+
 
 # Game Loop
 while not exit_game:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             exit_game = True
 
     gameWindow.fill(white)
+    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
+
+
 
 pygame.quit()
 quit()
